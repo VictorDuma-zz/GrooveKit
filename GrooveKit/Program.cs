4b027300574d09ac8f76199859c89128a221b18a
Viktor Duma <kvant.cv@gmail.com>
2018-07-11 15:30:32 -0400

    first commit

---
+++ GrooveKit/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Threading;
+using GHIElectronics.TinyCLR.Devices.Gpio;
+using GHIElectronics.TinyCLR.Pins;
+using GHIElectronics.TinyCLR.Networking.SPWF04Sx;
+using GHIElectronics.TinyCLR.Devices.Spi;
+using System.Text;
+using System.Diagnostics;
+using SeeedGroveStarterKit;
+
+namespace GrooveKit {
+    class Program {
+        private static GpioPin garageLed;
+        private static SPWF04SxInterface wifi;
+        private static bool connected;
+        private static bool isDoorOpened;
+        private static bool socketOpened;
+        private static int id;
+        public static string host;
+        public static int port;
+
+        static void Main() {
+            TouchSensor touch = new TouchSensor(FEZ.GpioPin.D7);
+            LightSensor light = new LightSensor(FEZ.AdcChannel.A3);
+            ServoMotor servo = new ServoMotor(FEZ.PwmPin.Controller4.Id, FEZ.PwmPin.Controller4.D5);
+            Buzzer buzz = new Buzzer(FEZ.GpioPin.D4);
+            LcdRgbBacklight lcd = new LcdRgbBacklight();
+
+            host = "192.168.1.152";
+            port = 80;
+
+            var buffer = new byte[512];
+            var cont = GpioController.GetDefault();
+            var reset = cont.OpenPin(FEZ.GpioPin.WiFiReset);
+            var irq = cont.OpenPin(FEZ.GpioPin.WiFiInterrupt);
+            var spi = SpiDevice.FromId(FEZ.SpiBus.WiFi, SPWF04SxInterface.GetConnectionSettings(FEZ.GpioPin.WiFiChipSelect));
+
+            connected = false;
+            socketOpened = false;
+            garageLed = cont.OpenPin(FEZ.GpioPin.D2);
+            servo.SetPosition(180);
+            garageLed.SetDriveMode(GpioPinDriveMode.Output);
+
+            wifi = new SPWF04SxInterface(spi, irq, reset);
+
+            wifi.IndicationReceived += (s, e) => Debug.WriteLine($"WIND: {Program.WindToName(e.Indication)} {e.Message}");
+            wifi.ErrorReceived += (s, e) => Debug.WriteLine($"ERROR: {e.Error} {e.Message}");
+
+            wifi.TurnOn();
+            //wifi.JoinNetwork("GHI", "ghi555wifi.");
+
+            lcd.Clear();
+            lcd.SetBacklightRGB(100, 100, 100);
+            lcd.Write("Time:");
+
+            while (!connected) {
+                ListenWind();
+                Thread.Sleep(200);
+            }
+
+            StringBuilder builder = new StringBuilder();
+
+            while (connected) {
+                if (!socketOpened) {
+                    id = wifi.OpenSocket(host, port, SPWF04SxConnectionyType.Tcp, SPWF04SxConnectionSecurityType.None);
+                    socketOpened = true;
+                }
+
+                var hour = DateTime.UtcNow.Hour;
+                var minute = DateTime.UtcNow.Minute;
+                var second = DateTime.UtcNow.Second;
+                lcd.SetCursor(7, 1);
+                lcd.Write($"{hour}:{minute}:{second}");
+
+                if (touch.IsTouched())
+                    wifi.WriteSocket(id, Encoding.UTF8.GetBytes("Someone wants to open the garage"));
+
+                if (light.ReadLightLevel() > 60 && isDoorOpened == true) {
+                    //Debug.WriteLine(light.ReadLightLevel().ToString());
+                    wifi.WriteSocket(id, Encoding.UTF8.GetBytes("Car in the garage"));
+                    while (light.ReadLightLevel() > 60)
+                        Thread.Sleep(50);
+                    wifi.WriteSocket(id, Encoding.UTF8.GetBytes("You can close the garage"));
+                }
+
+                if (wifi.QuerySocket(id) is var avail && avail > 0) {
+                    wifi.ReadSocket(id, buffer, 0, Math.Min(avail, buffer.Length));
+
+                    for (var k = 0; k < buffer.Length; k++) {
+                        if (buffer[k] != 0) {
+                            char result = (char)buffer[k];
+                            builder.Append(result);
+                            buffer[k] = 0;
+                        }
+                    }
+                    Debug.WriteLine(builder.ToString());
+                }
+                string command = builder.ToString();
+                builder.Clear();
+
+                switch (command) {
+                    case "open":
+                        buzz.Beep();
+                        servo.SetPosition(0);
+                        garageLed.Write(GpioPinValue.High);
+                        isDoorOpened = true;
+                        break;
+                    case "close":
+                        buzz.Beep();
+                        servo.SetPosition(180);
+                        garageLed.Write(GpioPinValue.Low);
+                        break;
+                    default:
+                        break;
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        public static void ListenWind() {
+            wifi.IndicationReceived += WaitForEvents;
+        }
+
+        public static void WaitForEvents(object sender, SPWF04SxIndicationReceivedEventArgs e) {
+            if (e.Indication == SPWF04SxIndication.NtpServerDelivery && connected == false) {
+                connected = true;
+                Debug.WriteLine("Connected");
+            }
+            if (e.Indication == SPWF04SxIndication.SocketClosed) {
+                socketOpened = false;
+                Debug.WriteLine("Connection lost");
+            }
+        }
+
+        private static string WindToName(SPWF04SxIndication wind) {
+            switch (wind) {
+                case SPWF04SxIndication.ConsoleActive: return nameof(SPWF04SxIndication.ConsoleActive);
+                case SPWF04SxIndication.PowerOn: return nameof(SPWF04SxIndication.PowerOn);
+                case SPWF04SxIndication.Reset: return nameof(SPWF04SxIndication.Reset);
+                case SPWF04SxIndication.WatchdogRunning: return nameof(SPWF04SxIndication.WatchdogRunning);
+                case SPWF04SxIndication.LowMemory: return nameof(SPWF04SxIndication.LowMemory);
+                case SPWF04SxIndication.WiFiHardwareFailure: return nameof(SPWF04SxIndication.WiFiHardwareFailure);
+                case SPWF04SxIndication.ConfigurationFailure: return nameof(SPWF04SxIndication.ConfigurationFailure);
+                case SPWF04SxIndication.HardFault: return nameof(SPWF04SxIndication.HardFault);
+                case SPWF04SxIndication.StackOverflow: return nameof(SPWF04SxIndication.StackOverflow);
+                case SPWF04SxIndication.MallocFailed: return nameof(SPWF04SxIndication.MallocFailed);
+                case SPWF04SxIndication.RadioStartup: return nameof(SPWF04SxIndication.RadioStartup);
+                case SPWF04SxIndication.WiFiPSMode: return nameof(SPWF04SxIndication.WiFiPSMode);
+                case SPWF04SxIndication.Copyright: return nameof(SPWF04SxIndication.Copyright);
+                case SPWF04SxIndication.WiFiBssRegained: return nameof(SPWF04SxIndication.WiFiBssRegained);
+                case SPWF04SxIndication.WiFiSignalLow: return nameof(SPWF04SxIndication.WiFiSignalLow);
+                case SPWF04SxIndication.WiFiSignalOk: return nameof(SPWF04SxIndication.WiFiSignalOk);
+                case SPWF04SxIndication.BootMessages: return nameof(SPWF04SxIndication.BootMessages);
+                case SPWF04SxIndication.KeytypeNotImplemented: return nameof(SPWF04SxIndication.KeytypeNotImplemented);
+                case SPWF04SxIndication.WiFiJoin: return nameof(SPWF04SxIndication.WiFiJoin);
+                case SPWF04SxIndication.WiFiJoinFailed: return nameof(SPWF04SxIndication.WiFiJoinFailed);
+                case SPWF04SxIndication.WiFiScanning: return nameof(SPWF04SxIndication.WiFiScanning);
+                case SPWF04SxIndication.ScanBlewUp: return nameof(SPWF04SxIndication.ScanBlewUp);
+                case SPWF04SxIndication.ScanFailed: return nameof(SPWF04SxIndication.ScanFailed);
+                case SPWF04SxIndication.WiFiUp: return nameof(SPWF04SxIndication.WiFiUp);
+                case SPWF04SxIndication.WiFiAssociationSuccessful: return nameof(SPWF04SxIndication.WiFiAssociationSuccessful);
+                case SPWF04SxIndication.StartedAP: return nameof(SPWF04SxIndication.StartedAP);
+                case SPWF04SxIndication.APStartFailed: return nameof(SPWF04SxIndication.APStartFailed);
+                case SPWF04SxIndication.StationAssociated: return nameof(SPWF04SxIndication.StationAssociated);
+                case SPWF04SxIndication.DhcpReply: return nameof(SPWF04SxIndication.DhcpReply);
+                case SPWF04SxIndication.WiFiBssLost: return nameof(SPWF04SxIndication.WiFiBssLost);
+                case SPWF04SxIndication.WiFiException: return nameof(SPWF04SxIndication.WiFiException);
+                case SPWF04SxIndication.WiFiHardwareStarted: return nameof(SPWF04SxIndication.WiFiHardwareStarted);
+                case SPWF04SxIndication.WiFiNetwork: return nameof(SPWF04SxIndication.WiFiNetwork);
+                case SPWF04SxIndication.WiFiUnhandledEvent: return nameof(SPWF04SxIndication.WiFiUnhandledEvent);
+                case SPWF04SxIndication.WiFiScan: return nameof(SPWF04SxIndication.WiFiScan);
+                case SPWF04SxIndication.WiFiUnhandledIndication: return nameof(SPWF04SxIndication.WiFiUnhandledIndication);
+                case SPWF04SxIndication.WiFiPoweredDown: return nameof(SPWF04SxIndication.WiFiPoweredDown);
+                case SPWF04SxIndication.HWInMiniAPMode: return nameof(SPWF04SxIndication.HWInMiniAPMode);
+                case SPWF04SxIndication.WiFiDeauthentication: return nameof(SPWF04SxIndication.WiFiDeauthentication);
+                case SPWF04SxIndication.WiFiDisassociation: return nameof(SPWF04SxIndication.WiFiDisassociation);
+                case SPWF04SxIndication.WiFiUnhandledManagement: return nameof(SPWF04SxIndication.WiFiUnhandledManagement);
+                case SPWF04SxIndication.WiFiUnhandledData: return nameof(SPWF04SxIndication.WiFiUnhandledData);
+                case SPWF04SxIndication.WiFiUnknownFrame: return nameof(SPWF04SxIndication.WiFiUnknownFrame);
+                case SPWF04SxIndication.Dot11Illegal: return nameof(SPWF04SxIndication.Dot11Illegal);
+                case SPWF04SxIndication.WpaCrunchingPsk: return nameof(SPWF04SxIndication.WpaCrunchingPsk);
+                case SPWF04SxIndication.WpaTerminated: return nameof(SPWF04SxIndication.WpaTerminated);
+                case SPWF04SxIndication.WpaStartFailed: return nameof(SPWF04SxIndication.WpaStartFailed);
+                case SPWF04SxIndication.WpaHandshakeComplete: return nameof(SPWF04SxIndication.WpaHandshakeComplete);
+                case SPWF04SxIndication.GpioInterrupt: return nameof(SPWF04SxIndication.GpioInterrupt);
+                case SPWF04SxIndication.Wakeup: return nameof(SPWF04SxIndication.Wakeup);
+                case SPWF04SxIndication.PendingData: return nameof(SPWF04SxIndication.PendingData);
+                case SPWF04SxIndication.InputToRemote: return nameof(SPWF04SxIndication.InputToRemote);
+                case SPWF04SxIndication.OutputFromRemote: return nameof(SPWF04SxIndication.OutputFromRemote);
+                case SPWF04SxIndication.SocketClosed: return nameof(SPWF04SxIndication.SocketClosed);
+                case SPWF04SxIndication.IncomingSocketClient: return nameof(SPWF04SxIndication.IncomingSocketClient);
+                case SPWF04SxIndication.SocketClientGone: return nameof(SPWF04SxIndication.SocketClientGone);
+                case SPWF04SxIndication.SocketDroppingData: return nameof(SPWF04SxIndication.SocketDroppingData);
+                case SPWF04SxIndication.RemoteConfiguration: return nameof(SPWF04SxIndication.RemoteConfiguration);
+                case SPWF04SxIndication.FactoryReset: return nameof(SPWF04SxIndication.FactoryReset);
+                case SPWF04SxIndication.LowPowerMode: return nameof(SPWF04SxIndication.LowPowerMode);
+                case SPWF04SxIndication.GoingIntoStandby: return nameof(SPWF04SxIndication.GoingIntoStandby);
+                case SPWF04SxIndication.ResumingFromStandby: return nameof(SPWF04SxIndication.ResumingFromStandby);
+                case SPWF04SxIndication.GoingIntoDeepSleep: return nameof(SPWF04SxIndication.GoingIntoDeepSleep);
+                case SPWF04SxIndication.ResumingFromDeepSleep: return nameof(SPWF04SxIndication.ResumingFromDeepSleep);
+                case SPWF04SxIndication.StationDisassociated: return nameof(SPWF04SxIndication.StationDisassociated);
+                case SPWF04SxIndication.SystemConfigurationUpdated: return nameof(SPWF04SxIndication.SystemConfigurationUpdated);
+                case SPWF04SxIndication.RejectedFoundNetwork: return nameof(SPWF04SxIndication.RejectedFoundNetwork);
+                case SPWF04SxIndication.RejectedAssociation: return nameof(SPWF04SxIndication.RejectedAssociation);
+                case SPWF04SxIndication.WiFiAuthenticationTimedOut: return nameof(SPWF04SxIndication.WiFiAuthenticationTimedOut);
+                case SPWF04SxIndication.WiFiAssociationTimedOut: return nameof(SPWF04SxIndication.WiFiAssociationTimedOut);
+                case SPWF04SxIndication.MicFailure: return nameof(SPWF04SxIndication.MicFailure);
+                case SPWF04SxIndication.UdpBroadcast: return nameof(SPWF04SxIndication.UdpBroadcast);
+                case SPWF04SxIndication.WpsGeneratedDhKeyset: return nameof(SPWF04SxIndication.WpsGeneratedDhKeyset);
+                case SPWF04SxIndication.WpsEnrollmentAttemptTimedOut: return nameof(SPWF04SxIndication.WpsEnrollmentAttemptTimedOut);
+                case SPWF04SxIndication.SockdDroppingClient: return nameof(SPWF04SxIndication.SockdDroppingClient);
+                case SPWF04SxIndication.NtpServerDelivery: return nameof(SPWF04SxIndication.NtpServerDelivery);
+                case SPWF04SxIndication.DhcpFailedToGetLease: return nameof(SPWF04SxIndication.DhcpFailedToGetLease);
+                case SPWF04SxIndication.MqttPublished: return nameof(SPWF04SxIndication.MqttPublished);
+                case SPWF04SxIndication.MqttClosed: return nameof(SPWF04SxIndication.MqttClosed);
+                case SPWF04SxIndication.WebSocketData: return nameof(SPWF04SxIndication.WebSocketData);
+                case SPWF04SxIndication.WebSocketClosed: return nameof(SPWF04SxIndication.WebSocketClosed);
+                case SPWF04SxIndication.FileReceived: return nameof(SPWF04SxIndication.FileReceived);
+                default: return "Other";
+            }
+        }
+    }
+}
---
+++ GrooveKit/SeeedGroveStarterKit.cs
@@ -0,0 +1,365 @@
+﻿using GHIElectronics.TinyCLR.Devices.Gpio;
+using GHIElectronics.TinyCLR.Devices.Adc;
+using System;
+using System.Threading;
+using GHIElectronics.TinyCLR.Devices.Pwm;
+using GHIElectronics.TinyCLR.Devices.I2c;
+using GHIElectronics.TinyCLR.Pins;
+
+namespace SeeedGroveStarterKit
+{
+    public class LightSensor
+    {
+        private AdcChannel Channel;
+        public LightSensor(int AdcPinNumber)
+        {
+            Channel = AdcController.GetDefault().OpenChannel(AdcPinNumber);
+        }
+        // between 0 and 100
+        public double ReadLightLevel()
+        {
+            return Channel.ReadRatio() * 100;
+        }
+    }
+
+    public class TouchSensor
+    {
+        GpioPin Pin;
+        public TouchSensor(int PinNumber)
+        {
+            Pin = GpioController.GetDefault().OpenPin(PinNumber);
+            Pin.Write(GpioPinValue.Low);
+            Pin.SetDriveMode(GpioPinDriveMode.Input);
+            Pin.ValueChanged += Pin_ValueChanged;
+        }
+
+        public bool IsTouched()
+        {
+            return Pin.Read() == GpioPinValue.High;
+        }
+
+
+        /// <summary>
+        /// The signature of button events.
+        /// </summary>
+        public delegate void TouchEventHandler();
+
+        /// <summary>
+        /// The event raised when a button is released.
+        /// </summary>
+        public event TouchEventHandler Touched;
+        public event TouchEventHandler Untouched;
+        private void Pin_ValueChanged(object sender, GpioPinValueChangedEventArgs e)
+        {
+            if (e.Edge == GpioPinEdge.RisingEdge)
+                Touched?.Invoke();
+            else
+                Untouched?.Invoke();
+        }
+
+    }
+
+    public class ServoMotor
+    {
+        private PwmPin servo;
+        public ServoMotor(string controller, int PwmPinNumber)
+        {
+            PwmController PWM = PwmController.FromId(controller);
+
+            servo = PWM.OpenPin(PwmPinNumber);
+            PWM.SetDesiredFrequency(1 / 0.020);
+        }
+        public double MinPulseCalibration
+        {
+            set
+            {
+                if (value > 1.5 || value < 0.1)
+                    throw new ArgumentOutOfRangeException("Must be between 0.1 and 1.5ms");
+                _MinPulseCalibration = value;
+            }
+        }
+        public double MaxPulseCalibration
+        {
+            set
+            {
+                if (value > 3 || value < 1.6)
+                    throw new ArgumentOutOfRangeException("Must be between 1.6 and 3ms");
+                _MaxPulseCalibration = value;
+            }
+        }
+        // min and max pulse width in milliseconds
+        private double _MinPulseCalibration = 1.0;
+        private double _MaxPulseCalibration = 2.0;
+
+        /// <summary>
+        /// Sets the position of the Servo Motor.
+        /// </summary>
+        /// <param name="position">The position of the servo between 0 and 180 degrees.</param>
+        public void SetPosition(double position)
+        {
+            if (position < 0 || position > 180) throw new ArgumentOutOfRangeException("degrees", "degrees must be between 0 and 180.");
+
+            // Typically, with 50 hz, 0 degree is 0.05 and 180 degrees is 0.10
+            //double duty = ((position / 180.0) * (0.10 - 0.05)) + 0.05;
+            double duty = ((position / 180.0) * (_MaxPulseCalibration / 20 - _MinPulseCalibration / 20)) + _MinPulseCalibration / 20;
+
+
+            servo.SetActiveDutyCyclePercentage(duty);
+            servo.Start();
+        }
+    }
+
+    public class Buzzer
+    {
+        private GpioPin Pin;
+        public Buzzer(int GpioPinNumber)
+        {
+            Pin = GpioController.GetDefault().OpenPin(GpioPinNumber);
+            Pin.Write(GpioPinValue.Low);
+            Pin.SetDriveMode(GpioPinDriveMode.Output);
+        }
+        public void TurnOn()
+        {
+            Pin.Write(GpioPinValue.High);
+        }
+        public void TurnOff()
+        {
+            Pin.Write(GpioPinValue.Low);
+        }
+        public void Beep()
+        {
+            TurnOn();
+            Thread.Sleep(10);
+            TurnOff();
+        }
+    }
+
+    public class LcdRgbBacklight
+    {
+        private I2cDevice DisplayDevice;
+        private I2cDevice BacklightDevice;
+
+        private byte _displayfunction;
+        private byte _displaycontrol;
+        private byte _displaymode;
+        private byte _numlines, _currline;
+
+
+        private byte LCD_CLEARDISPLAY = 0x01;
+        private byte LCD_RETURNHOME = 0x02;
+        private byte LCD_ENTRYMODESET = 0x04;
+        private byte LCD_DISPLAYCONTROL = 0x08;
+        private byte LCD_CURSORSHIFT = 0x10;
+        private byte LCD_FUNCTIONSET = 0x20;
+        private byte LCD_SETCGRAMADDR = 0x40;
+        private byte LCD_SETDDRAMADDR = 0x80;
+
+        private byte LCD_DISPLAYON = 0x04;
+        private byte LCD_DISPLAYOFF = 0x00;
+        private byte LCD_CURSORON = 0x02;
+        private byte LCD_CURSOROFF = 0x00;
+        private byte LCD_BLINKON = 0x01;
+        private byte LCD_BLINKOFF = 0x00;
+        private byte LCD_ENTRYRIGHT = 0x00;
+        private byte LCD_ENTRYLEFT = 0x02;
+        private byte LCD_ENTRYSHIFTINCREMENT = 0x01;
+        private byte LCD_ENTRYSHIFTDECREMENT = 0x00;
+
+        private byte REG_MODE1 = 0x00;
+        private byte REG_MODE2 = 0x01;
+        private byte REG_OUTPUT = 0x08;
+
+        public LcdRgbBacklight()
+        {
+            var settings = new I2cConnectionSettings((0x7c >> 1));
+            settings.SharingMode = I2cSharingMode.Shared;
+            settings.BusSpeed = I2cBusSpeed.FastMode;
+
+            //string aqs = I2cDevice.GetDeviceSelector("I2C1");
+            DisplayDevice = I2cDevice.FromId(FEZ.I2cBus.I2c1, settings);
+
+            settings = new I2cConnectionSettings((0xc4 >> 1));
+            settings.SharingMode = I2cSharingMode.Shared;
+            settings.BusSpeed = I2cBusSpeed.FastMode;
+
+            BacklightDevice = I2cDevice.FromId(FEZ.I2cBus.I2c1, settings);
+
+            ////////////////////////////////////
+            // get the display going
+
+            //byte cols = 6;
+            byte lines = 2;
+            byte dotsize = 0;
+
+
+            if (lines > 1)
+            {
+                _displayfunction |= 0x08;// LCD_2LINE;
+            }
+            _numlines = lines;
+            _currline = 0;
+
+            // for some 1 line displays you can select a 10 pixel high font
+            if ((dotsize != 0) && (lines == 1))
+            {
+                _displayfunction |= 0x04;// LCD_5x10DOTS;
+            }
+
+            // SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
+            // according to datasheet, we need at least 40ms after power rises above 2.7V
+            // before sending commands. Arduino can turn on way befer 4.5V so we'll wait 50
+            //delayMicroseconds(50000);
+            Thread.Sleep(50);
+
+            // this is according to the hitachi HD44780 datasheet
+            // page 45 figure 23
+
+            // Send function set command sequence
+            command((byte)(LCD_FUNCTIONSET | _displayfunction));
+            //delayMicroseconds(4500);  // wait more than 4.1ms
+            Thread.Sleep(5);
+
+            // second try
+            command((byte)(LCD_FUNCTIONSET | _displayfunction));
+            //delayMicroseconds(150);
+            Thread.Sleep(1);
+
+            // third go
+            command((byte)(LCD_FUNCTIONSET | _displayfunction));
+
+
+            // finally, set # lines, font size, etc.
+            command((byte)(LCD_FUNCTIONSET | _displayfunction));
+
+            // turn the display on with no cursor or blinking default
+            _displaycontrol = (byte)(LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF);
+            EnableDisplay(true);
+
+            // clear it off
+            Clear();
+
+            // Initialize to default text direction (for romance languages)
+            _displaymode = (byte)(LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT);
+            // set the entry mode
+            command((byte)(LCD_ENTRYMODESET | _displaymode));
+
+
+            // backlight init
+            WriteBacklightReg(REG_MODE1, 0);
+            // set LEDs controllable by both PWM and GRPPWM registers
+            WriteBacklightReg(REG_OUTPUT, 0xFF);
+            // set MODE2 values
+            // 0010 0000 -> 0x20  (DMBLNK to 1, ie blinky mode)
+            WriteBacklightReg(REG_MODE2, 0x20);
+
+            //setColorWhite();
+            SetBacklightRGB(255, 0, 100);
+
+        }
+
+         // send command
+        private void command(byte value)
+        {
+            byte[] dta = new byte[2] { 0x80, value };
+            DisplayDevice.Write(dta);
+            //i2c_send_byteS(dta, 2);
+        }
+
+        // send data
+        private void write(byte value)
+        {
+
+            byte[] dta = new byte[2] { 0x40, value };
+            //i2c_send_byteS(dta, 2);
+            DisplayDevice.Write(dta);
+            //return 1; // assume sucess
+        }
+
+        /********** high level commands, for the user! */
+
+        public void Write(string s)
+        {
+
+            for (int i = 0; i < s.Length; i++)
+                write((byte)s[i]);
+        }
+        public void Clear()
+        {
+            command(LCD_CLEARDISPLAY);        // clear display, set cursor position to zero
+            //delayMicroseconds(2000);          // this command takes a long time!
+            Thread.Sleep(2);
+        }
+
+        public void GoHome()
+        {
+            command(LCD_RETURNHOME);        // set cursor position to zero
+            //delayMicroseconds(2000);        // this command takes a long time!
+            Thread.Sleep(2);
+        }
+
+        public void SetCursor(byte col, byte row)
+        {
+
+            col = (byte)(row == 0 ? (col | 0x80) : (col | 0xc0));
+            command(col);
+            //byte[] dta = new byte[2] { 0x80, col };
+            //DisplayDevice.Write(dta);
+
+            //i2c_send_byteS(dta, 2);
+
+        }
+
+        // Turn the display on/off (quickly)
+        public void EnableDisplay(bool on)
+        {
+            if (on)
+                _displaycontrol |= LCD_DISPLAYON;
+            else
+                _displaycontrol &= (byte)~LCD_DISPLAYON;
+
+            command((byte)(LCD_DISPLAYCONTROL | _displaycontrol));
+        }
+
+        // =============================================================================
+
+        // Control the backlight LED blinking
+        private void WriteBacklightReg(byte addr, byte data)
+        {
+            byte[] wb = new byte[2];
+            wb[0] = addr;
+            wb[1] = data;
+
+            BacklightDevice.Write(wb);
+
+            //I2C.WriteRead((0xc4 >> 1), wb, 0, 2, rb, 0, 0, out written, out read);
+
+        }
+
+        public void BlinkBacklight(bool on)
+        {
+            // blink period in seconds = (<reg 7> + 1) / 24
+            // on/off ratio = <reg 6> / 256
+            if (on)
+            {
+                WriteBacklightReg(0x07, 0x17);  // blink every second
+                WriteBacklightReg(0x06, 0x7f);  // half on, half off
+            }
+            else
+            {
+                WriteBacklightReg(0x07, 0x00);
+                WriteBacklightReg(0x06, 0xff);
+            }
+        }
+        public void SetBacklightRGB(byte r, byte g, byte b)
+        {
+
+            byte REG_RED = 0x04;    // pwm2
+            byte REG_GREEN = 0x03;      // pwm1
+            byte REG_BLUE = 0x02;      // pwm0
+
+            WriteBacklightReg(REG_RED, r);
+            WriteBacklightReg(REG_GREEN, g);
+            WriteBacklightReg(REG_BLUE, b);
+        }
+    }
+}
